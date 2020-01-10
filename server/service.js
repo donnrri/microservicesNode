14a06f13005787a43b719104b767c6632b82c1bd
davet <daveenq@gmail.com>
2020-01-10 17:35:46 +0000

    Imported ServiceRegistry and used it in get serviceRegister handler

--- server/service.js
+++ server/service.js
@@ -1,43 +1,47 @@
 const express = require('express');
+const ServiceRegistry = require('./lib/ServiceRegistry');
 
 const service = express();
-// const ServiceRegistry = require('./ServiceRegistry');
+
 
 module.exports = (config) => {
-  const log = config.log();
-  // Add a request logging middleware in development mode
-  if (service.get('env') === 'development') {
-    service.use((req, res, next) => {
-      log.debug(`${req.method}: ${req.url}`);
-      return next();
+    const log = config.log();
+    // Add a request logging middleware in development mode
+    const serviceRegistry = new ServiceRegistry(log);
+    if (service.get('env') === 'development') {
+        service.use((req, res, next) => {
+            log.debug(`${req.method}: ${req.url}`);
+            return next();
+        });
+    }
+
+    // Add api to discovery and use the service registry
+
+    service.get('find/:servicename/serviceversion', (req, res, next) => {
+        const {servicename, serviceversion, serviceport} = req.params;
+        // on some systems remote ip is in IPv6 notation
+        // code below handles that eventuality
+        const serviceip = req.connection.remoteAddress.includes('::') ? `[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
+        const result = serviceRegistry.register(servicename, serviceversion, serviceip, serviceport);
+        return res.json({ result });
+
     });
-  }
 
-  // Add api to discovery and use the service registry
+    service.put('resgister/:servicename/serviceversion/:serviceport', (req, res, next) => next('Not Implemented'));
 
-  service.get('find/:servicename/serviceversion', (req, res, next) => {
-    return next('Not Implemented');
-  });
-
-  service.put('resgister/:servicename/serviceversion/:serviceport', (req, res, next) => {
-    return next('Not Implemented');
-  });
-
-  service.delete('resgister/:servicename/serviceversion/:serviceport', (req, res, next) => {
-    return next('Not Implemented');
-  });
+    service.delete('resgister/:servicename/serviceversion/:serviceport', (req, res, next) => next('Not Implemented'));
 
 
-  // eslint-disable-next-line no-unused-vars
-  service.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    // Log out the error to the console
-    log.error(error);
-    return res.json({
-      error: {
-        message: error.message,
-      },
+    // eslint-disable-next-line no-unused-vars
+    service.use((error, req, res, next) => {
+        res.status(error.status || 500);
+        // Log out the error to the console
+        log.error(error);
+        return res.json({
+            error: {
+                message: error.message,
+            },
+        });
     });
-  });
-  return service;
+    return service;
 };
